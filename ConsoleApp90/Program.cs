f56620bab33dd42a95339ca2154537ada5d046d9
NHOY <NHOY@2210DR000919>
2023-07-08 21:54:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp90/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp90
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol;
+            int bracketExpression = 0;
+            int depthExpression = 0;
+            bool isBracketExpressionCorrect = true;
+
+            Console.Write("Введите скобочное выражение:");
+            symbol = Console.ReadLine();
+
+            for (int i = 0; i < symbol.Length; i++)
+            {
+                if (symbol[i] == '(' && bracketExpression == 0)
+                {
+                    bracketExpression++;
+                    depthExpression++;
+                }
+                else if (symbol[i] == ')' && bracketExpression > 0)
+                {
+                    bracketExpression--;
+                }
+                else if ((symbol[i] == '(') && depthExpression > 0)
+                {
+                    isBracketExpressionCorrect = false;
+                }
+            }
+
+            Console.WriteLine(bracketExpression);
+
+            if (bracketExpression == 0 && isBracketExpressionCorrect == true)
+            {
+                Console.WriteLine("Скобочное выражение - верное.");
+            }
+            else if (isBracketExpressionCorrect == false)
+            {
+                Console.WriteLine("Скобочное выражение - не верное.");
+            }
+
+            Console.WriteLine("Максимальная глубина скобочного выражения - " + depthExpression);
+        }
+    }
+}
